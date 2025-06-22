9d5425ce1ee4504e8b1930f9c0021004f9dcf60c
saraNasser4 <saransser6@gmail.com>
2025-06-22 14:36:15 +0300

    [2] connecting between Next and .net

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Adding The main colors
 
 Thought it would be better to start fresh with the color so here we are.
+
+## Connect between them
+
+I started with connect the two part of the project which each other in order to the things go smooth on the production mode.
--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+const backendPort = process.env.BACKEND_PORT || '5065';
 const nextConfig: NextConfig = {
   /* config options here */
+    async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `http://localhost:${backendPort}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
+
